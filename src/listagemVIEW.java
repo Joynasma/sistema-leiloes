eef0d3655d0de16e7cc1a4d03a85b61ee0b9e7a6
“Joyce <joyce@gmail.com>
2024-10-24 09:56:58 -0300

    Implementada a funcionalidade de venda na tela de listagem

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -137,12 +137,11 @@ public class listagemVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
-        String id = id_produto_venda.getText();
-        
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
-        //produtosdao.venderProduto(Integer.parseInt(id));
-        listarProdutos();
+        int idProduto = Integer.parseInt(id_produto_venda.getText());
+        ProdutosDAO produtosDAO = new ProdutosDAO();
+        produtosDAO.venderProduto(idProduto);
+
+        listarProdutos(); // Atualiza a listagem após a venda
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
